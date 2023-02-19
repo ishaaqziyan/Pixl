4ee7f6a3ef5fb6193419823e7a8bc256d5cfe42a
Ishaaq <ishaaqziyan@gmail.com>
2023-02-19 13:55:14 +0530

    Further Improvements for the brush functionality

--- pxcanvas/mouse.go
+++ pxcanvas/mouse.go
@@ -3,14 +3,18 @@ package pxcanvas
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/driver/desktop"
+	"zerotomastery.io/pixl/pxcanvas/brush"
 )
 
-func (pxCanvas *PxCanvas) scrolled(ev *fyne.ScrollEvent) {
+func (pxCanvas *PxCanvas) Scrolled(ev *fyne.ScrollEvent) {
 	pxCanvas.scale(int(ev.Scrolled.DY))
 	pxCanvas.Refresh()
 }
 
 func (pxCanvas *PxCanvas) MouseMoved(ev *desktop.MouseEvent) {
+	if x, y := pxCanvas.MouseToCanvasXY(ev); x != nil && y != nil {
+		brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
+	}
 	pxCanvas.TryPan(pxCanvas.mouseState.previousCoord, ev)
 	pxCanvas.Refresh()
 	pxCanvas.mouseState.previousCoord = &ev.PointEvent
@@ -18,3 +22,8 @@ func (pxCanvas *PxCanvas) MouseMoved(ev *desktop.MouseEvent) {
 
 func (pxCanvas *PxCanvas) MouseIn(ev *desktop.MouseEvent) {}
 func (pxCanvas *PxCanvas) MouseOut()                      {}
+
+func (pxCanvas *PxCanvas) MouseUp(ev *desktop.MouseEvent) {}
+func (pxCanvas *PxCanvas) MouseDown(ev *desktop.MouseEvent) {
+	brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
+}
